9781562541c0716ad1e86dc76698625708c5d0fb
naoufal51 <naoufal51@gmail.com>
2023-08-20 18:05:23 +0100

    Add dist for webapp and Docs

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# sparkify_proj
+# Sparkify Churn Detection Project
+
+Sparkify Churn Detection is a project designed to predict user churn on the fictitious Sparkify digital music service platform. The model would be crucial for the stakeholders at Sparkify to devise appropriate user retention strategies. The project features an interactive web dashboard and provides the capability to train the churn detection model using predefined configurations.
+
+## Getting Started
+
+### Web Application Setup:
+Follow the steps below to set up and run the web dashboard:
+
+1. Export the Weights & Biases (W&B) API key:
+```sh
+export WANDB_API_KEY=YOUR_WANDB_API_KEY
+```
+
+2. Set up the dashboard:
+```sh
+cd app/dashboard
+echo "VITE_API_URL=http://127.0.0.1:8080" > .env
+npm install
+npm run build
+cd ..
+```
+3. Start the application:
+```sh
+uvicorn main:app --host 127.0.0.1 --port 8080
+```
+
+
+
+## Model Training:
+For a detailed walkthrough of model training, check the dedicated [Kaggle notebook](https://www.kaggle.com/naoufal51/sparkify-project-run)
+
+Alternatively, set up the training environment using the following steps:
+
+1. Clone the repository and setup the env:
+``` sh 
+git clone https://github.com/naoufal51/sparkify_proj.git
+pip install -r sparkify_proj/requirements.txt
+```
+2. Update the config.toml file with your API keys and custom conf. For instance:
+```sh
+[model_experiment]
+max_evals = 20
+
+[wandb]
+key = "your_wandb_key"
+project = "sparkify_naoufal"
+
+[prefect]
+key = "your_prefect_key"
+
+[raw_data_s3]
+file_name = "sparkify/sparkify_event_data.json"
+
+```
+3. Train the model:
+``` sh 
+cd sparkify_proj/
+python main.py
+
+```
--- app/main.py
+++ app/main.py
@@ -23,8 +23,20 @@ from starlette.responses import FileResponse
 # threading lock
 spark_lock = Lock()
 
-wandb_key = os.environ.get('WANDB_API_KEY')
+wandb_key = os.environ.get("WANDB_API_KEY")
 wandb.login(key=wandb_key)
+
+wandb_project = "sparkify_naoufal"
+
+# Config pipeline
+pipeline_name = "pipeline_model"
+pipeline_version = "v0"
+pipeline_name_version = f"{pipeline_name}:{pipeline_version}"
+
+# Config Model
+classifier_name = "XGBClassifier.pkl"
+classifier_version = "v0"
+classifier = f"{classifier_name}:{classifier_version}"
 
 # define app
 app = FastAPI()
@@ -32,45 +44,34 @@ app = FastAPI()
 api_router = APIRouter()
 
 
-
-
-# CORS configuration
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Allow all origins
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-
-
 # define data schema
-schema = StructType([
-    StructField("artist", StringType(), True),
-    StructField("auth", StringType(), True),
-    StructField("gender", StringType(), True),
-    StructField("ts", LongType(), True),
-    StructField("userId", StringType(), True),
-    StructField("sessionId", LongType(), True),
-    StructField("page", StringType(), True),
-    StructField("method", StringType(), True),
-    StructField("status", LongType(), True),
-    StructField("level", StringType(), True),
-    StructField("itemInSession", LongType(), True),
-    StructField("location", StringType(), True),
-    StructField("userAgent", StringType(), True),
-    StructField("lastName", StringType(), True),
-    StructField("firstName", StringType(), True),
-    StructField("Song", StringType(), True),
-    StructField("registration", LongType(), True),
-    StructField("length", DoubleType(), True)
-])
+schema = StructType(
+    [
+        StructField("artist", StringType(), True),
+        StructField("auth", StringType(), True),
+        StructField("gender", StringType(), True),
+        StructField("ts", LongType(), True),
+        StructField("userId", StringType(), True),
+        StructField("sessionId", LongType(), True),
+        StructField("page", StringType(), True),
+        StructField("method", StringType(), True),
+        StructField("status", LongType(), True),
+        StructField("level", StringType(), True),
+        StructField("itemInSession", LongType(), True),
+        StructField("location", StringType(), True),
+        StructField("userAgent", StringType(), True),
+        StructField("lastName", StringType(), True),
+        StructField("firstName", StringType(), True),
+        StructField("Song", StringType(), True),
+        StructField("registration", LongType(), True),
+        StructField("length", DoubleType(), True),
+    ]
+)
 
 
 def remove_colon_from_dir_path(dir_path: str):
-    if ':' in dir_path:
-        new_dir_path = dir_path.replace(':', '_')
+    if ":" in dir_path:
+        new_dir_path = dir_path.replace(":", "_")
         if os.path.exists(new_dir_path):
             shutil.rmtree(new_dir_path)
         shutil.move(dir_path, new_dir_path)
@@ -80,26 +81,25 @@ def remove_colon_from_dir_path(dir_path: str):
 
 
 def load_artifacts():
-    with wandb.init(project='sparkify_v2') as run:
+    with wandb.init(project=wandb_project) as run:
         pipeline_model_path = run.use_artifact(
-            'pipeline_model:v0', type='pipeline_model').download()
+            pipeline_name_version, type="pipeline_model"
+        ).download()
         pipeline_model_path = remove_colon_from_dir_path(pipeline_model_path)
         pipeline_model = PipelineModel.load(pipeline_model_path)
 
-        model_path = run.use_artifact(
-            'XGBClassifier.pkl:v8', type='model').download()
-        model = joblib.load(model_path + '/XGBClassifier.pkl')
+        model_path = run.use_artifact(classifier, type="model").download()
+        model = joblib.load(model_path + f"/{classifier_name}")
 
     return pipeline_model, model
 
 
-
 def create_spark_session():
     import uuid
+
     # Generate a unique app name
     app_name = f"Sparkify_{uuid.uuid4()}"
-    spark = SparkSession.builder.master(
-        "local").appName(app_name).getOrCreate()
+    spark = SparkSession.builder.master("local").appName(app_name).getOrCreate()
 
     return spark
 
@@ -110,14 +110,15 @@ async def startup_event():
     pipeline_model, model = load_artifacts()
     spark = create_spark_session()
 
+
 @api_router.post("/testpost")
 def test_post():
     return {"message": "Post successful!"}
 
+
 @api_router.post("/predict")
 async def predict(user_data_file: UploadFile = File(...)):
     try:
-
         # Creating a temporary file
         with tempfile.NamedTemporaryFile(delete=True) as temp_file:
             temp_file.write(await user_data_file.read())
@@ -138,13 +139,12 @@ async def predict(user_data_file: UploadFile = File(...)):
             feature_engineered_data = feature_engineer.feature_engineering()
 
             # Run data through the pipeline
-            data_pipeline = DataPipeline(feature_engineered_data, './data')
-            data_pipeline_data = data_pipeline.run_inference_pipeline(
-                pipeline_model)
+            data_pipeline = DataPipeline(feature_engineered_data, "./data")
+            data_pipeline_data = data_pipeline.run_inference_pipeline(pipeline_model)
 
             # Separate userId from other features
-            user_id = data_pipeline_data['userId']
-            data_pipeline_data = data_pipeline_data.drop('userId', axis=1)
+            user_id = data_pipeline_data["userId"]
+            data_pipeline_data = data_pipeline_data.drop("userId", axis=1)
 
             # Predict the churn probability
             predictions = model.predict_proba(data_pipeline_data)
@@ -161,37 +161,39 @@ async def predict(user_data_file: UploadFile = File(...)):
 
             # Create a dictionary with userIds as keys and (churn_probability, SHAP_values) as values
             prediction_and_explanation = []
-            for uid, prob, shap_values in zip(user_id, churn_probabilities, shap_values_list):
+            for uid, prob, shap_values in zip(
+                user_id, churn_probabilities, shap_values_list
+            ):
                 # We convert the feature contributions into a dictionary to make it easier to interpret
-                contributions = dict(
-                    zip(data_pipeline_data.columns, shap_values))
+                contributions = dict(zip(data_pipeline_data.columns, shap_values))
                 prediction_and_explanation.append(
-                    {"userId": uid, "churn_probability": prob, "contributions": contributions})
+                    {
+                        "userId": uid,
+                        "churn_probability": prob,
+                        "contributions": contributions,
+                    }
+                )
 
             spark.stop()
 
-            return {'prediction': prediction_and_explanation}
+            return {"prediction": prediction_and_explanation}
 
     except Exception as e:
         # If any error occurred, stop the spark session and raise the exception
         raise HTTPException(status_code=400, detail=str(e))
 
+
 # serve dashboard (static file dist)
 app.include_router(api_router, prefix="/api")
 
 # app.mount("/", StaticFiles(directory="dashboard/dist", html=True), name="static")
-app.mount("/dashboard/static", StaticFiles(directory="dashboard/dist", html=True), name="static")
+app.mount(
+    "/dashboard/static",
+    StaticFiles(directory="dashboard/dist", html=True),
+    name="static",
+)
 
 
 @app.get("/dashboard")
 async def serve_index():
     return FileResponse("dashboard/dist/index.html")
-
-
-# @app.get("/")
-# async def serve_index():
-#     return FileResponse("dashboard/dist/index.html")
-
-
-# uvicorn app.main:app --host 127.0.0.1 --port 8080
-# npm run dev
